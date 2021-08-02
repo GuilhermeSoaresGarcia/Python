9b500d0983efe63f5921a48becb710ef59c35528
guidjelegamba <guidjelegamba@gmail.com>
2021-08-02 16:50:05 -0300

    ---

---
+++ Semana 5/func.py
@@ -0,0 +1,15 @@
+# def soma(x, y, z):
+#     return x + y + z
+
+# def multiplica(x, y, z):
+#     return x * y * z
+
+# def nome_do_seu_time ():
+#     return "Grêmio"
+
+# print(soma(10, 20, 30))
+
+def total_Caracteres (x, y, z):
+    return len(x)+len(y)+len(z)
+    
+print(total_Caracteres)
